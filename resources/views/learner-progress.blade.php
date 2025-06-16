6f3498f4ed7b7b87503b02c076b37b193070ffa6
Given Tshuma <given.tshuma@pmconnect.co.uk>
2025-06-16 14:52:40 +0200

    use a table instead of divs to show the learner progress data

--- resources/views/learner-progress.blade.php
+++ resources/views/learner-progress.blade.php
@@ -16,12 +16,15 @@
 </head>
 
 <body>
-    <div class="py-10">
+    <div class="py-10 px-4">
         <div>
-            <h1 class="text-blue font-black">Learner Progress Page</h1>
+            <h1 class="text-blue font-black text-center pb-2 text-4xl">Learner Progress Page</h1>
+            <hr class="py-2">
+            <div class="flex gap-2">
             <form action="/learner-progress" method="GET">
                 @csrf
-                <select name="course">
+                <div class="flex gap-2">
+                <select class="rounded-md border-2 border-solid border-amber-300 py-2" name="course">
                     <option value="">Course Filter</option>
                     @foreach ($courses as $course)
                     <option 
@@ -31,7 +34,7 @@
                         </option>
                     @endforeach
                 </select>
-                <select name="sort">
+                <select class="rounded-md border-2 border-solid border-amber-300 py-2" name="sort">
                     <option value="">Sorting Order</option>
                     <option value="asc" @if (isset($course) && $sortingOrder == 'asc') selected @endif>
                         Ascending
@@ -40,27 +43,45 @@
                         Descending
                     </option>
                 </select>
-                <button>Filter</button>
+                <button class="bg-gray-300 rounded-md px-4 py-2">Filter</button>
+                </div>
             </form>
             <form action="/learner-progress" method="GET">
-            <button>Clear Filters</button></form>
+            <button class="bg-gray-400 rounded-md px-4 py-2">Clear Filters</button>
+            </form>
+            </div>
         </div>
-        
-        @foreach ($learners as $learner)
-            <div>
-                <h3>{{ $learner->firstname }} {{ $learner->firstname }}</h3>
-            </div>
-            <div class="pl-2">
+        <table class="w-full">
+            <thead>
+                <tr>
+                    <th class="text-left sticky top-0 bg-white text-xl">Name</th>
+                    <th class="text-left pl-2 sticky top-0 bg-white text-xl">Surname</th>
+                    <th class="text-left sticky top-0 bg-white text-xl">Progress(%)</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($learners as $learner)
+            <tr>
+                <td class="font-bold">{{ $learner->firstname }}</td>
+                <td class="font-bold pl-2">{{ $learner->lastname }}</td>
+                <td class="font-bold">{{round($learner->avg(),2)}}</td>
+            </tr>
                 @foreach ($learner->courses as $course)
-                    <div class="flex gap-2">
-                        <p>{{ $course->name }}</p>
-                        <p>{{ $course->pivot->progress }}</p>
-                        <p>{{ $learner->avg() }}</p>
-                    </div>
+                <tr class="border-b-solid border-b-gray-100 border-b-[2px]">
+                    <td>{{ $course->name }}</td>
+                    <td class="pl-2"></td>
+                    <td>{{ $course->pivot->progress }}</td>
+                </tr>
                 @endforeach
-            </div>
+                <tr class="border-b-solid border-b-2 border-b-gray-300">
+                    <td></td>
+                    <td class="pl-2"></td>
+                    <td></td>
+                </tr>
         @endforeach
-        <div>
+            </tbody>
+        </table>
+        <div class="py-2">
             {{ $learners->appends(request()->input())->links() }}
         </div>
     </div>
